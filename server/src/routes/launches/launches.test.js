865ff6ce8201ddb76f92a033950a6912e666cb5b
Celine <celine.lancelle@gmail.com>
2023-07-03 17:20:15 +0200

    reduce number of environments tested

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,12 +1,14 @@
 const request = require("supertest");
 const app = require("../../app");
 const { mongoConnect, mongoDisconnect } = require("../../services/mongo");
+const { loadPlanetsData } = require("../../models/planets.model");
 
 const MONGO_URL = process.env.MONGO_URL;
 
 describe("launches API", () => {
   beforeAll(async () => {
     await mongoConnect();
+    await loadPlanetsData();
   });
 
   afterAll(async () => {
